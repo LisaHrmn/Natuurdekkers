161e85ea3cfae30a0db71677b88c2b89371310c5
JakkoAndeweg <jakko.andeweg@gmail.com>
2021-05-12 11:42:49 +0200

    First detail page has all elements to some degree

--- groendak/src/Details1/Details1.js
+++ groendak/src/Details1/Details1.js
@@ -1,13 +1,41 @@
 import './Details1.css';
 import placeholder from "./images/placeholder.png";
+import subplaceholder1 from "./images/sub-placeholder1.png";
+import subplaceholder2 from "./images/sub-placeholder2.png";
+import subplaceholder3 from "./images/sub-placeholder3.png";
 function Details1() {
   return (
     <div>
+    
+      {/* <img class="rtl" src={placeholder} alt="placeholder" width="100" height="50"></img> */}
       <a class="terug" href="/">terug</a>
-      <h1>Economish groen dak</h1>
+
+      <div class="row">
+      <div class="column3">
+    <a class= "arrow right" href="/"></a>
+  </div>
+  <div class="column2">
+  <a href="/"><img src={subplaceholder1}  alt="Snow" width="40" height="80"></img></a>  
+  </div>
+  <div class="column1">
+  <a href="/"><img src={subplaceholder2}  alt="Forest" width="50" height="100"></img></a>
+  </div>
+  <div class="column2">
+  <a href="/"><img src= {subplaceholder3} alt="Mountains" width="40" height="80"></img></a>
+  </div>
+  <div class="column2">
+    <a class= "arrow left" href="/"></a>
+  </div>
+</div>
+
+
 <div class="div2"><p class="wb">dit is hele interessante informatie over heel interessante dingen</p>
            <button class="sb">verder</button> 
       </div>
+
+      <h1>Economish groen dak</h1>
+
+
       <h2 class= "td">technische informatie</h2>
       <ul>
         <ul class="ulbl">gewicht</ul>
@@ -19,8 +47,16 @@ function Details1() {
         <ul class="ulbl">Waterbuffering</ul>
       </ul>
       
-      {/* <div class="lbr"> */}
-      <ul class= "ulr">
+      
+     
+     <div class="row2">
+
+    <div class="column4">
+    <i class= "arrow right"></i>
+    </div>
+
+     <div class="column5">
+      {/* <ul class= "ulr"> */}
   <ul><div class="gallery">
   <a target="_blank" href="img_5terre.jpg">
     <img src={placeholder} alt="placeholder" width="300" height="200"></img>
@@ -42,9 +78,16 @@ function Details1() {
 </div>
 </ul>
 
-</ul>
-{/* </div> */}
-      <img src={placeholder} alt="placeholder" width="400" height="200"></img>
+<div class="column6">
+    <i class= "arrow left"></i>
+    </div>
+
+
+{/* </ul> */}
+</div>
+      <div class= "gallery2"><img src={placeholder} alt="placeholder" width="400" height="200"></img></div>
+      
+      </div>
     </div>
   );
 }
