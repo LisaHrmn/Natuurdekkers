ae667e8d25f6885db2b32cddbfbe288acb281588
JakkoAndeweg <jakko.andeweg@gmail.com>
2021-05-12 10:09:19 +0200

    Details page 1 added

--- groendak/src/Details1/Details1.js
+++ groendak/src/Details1/Details1.js
@@ -1,9 +1,50 @@
 import './Details1.css';
-
+import placeholder from "./images/placeholder.png";
 function Details1() {
   return (
     <div>
-      <h1 class= "red" >Gedetailleerde informatie over groene daken</h1>
+      <a class="terug" href="/">terug</a>
+      <h1>Economish groen dak</h1>
+<div class="div2"><p class="wb">dit is hele interessante informatie over heel interessante dingen</p>
+           <button class="sb">verder</button> 
+      </div>
+      <h2 class= "td">technische informatie</h2>
+      <ul>
+        <ul class="ulbl">gewicht</ul>
+        <ul class="ulbl">systeemdikte</ul>
+        <ul class="ulbl">Dakhelling</ul>
+        <ul class="ulbl">Vegetatie</ul>
+        <ul class="ulbl">Waterretentie</ul>
+        <ul class="ulbl">Afvoercoëfficient</ul>
+        <ul class="ulbl">Waterbuffering</ul>
+      </ul>
+      
+      {/* <div class="lbr"> */}
+      <ul class= "ulr">
+  <ul><div class="gallery">
+  <a target="_blank" href="img_5terre.jpg">
+    <img src={placeholder} alt="placeholder" width="300" height="200"></img>
+  </a>
+</div>
+</ul>
+
+<ul><div class="gallery">
+  <a target="_blank" href="img_5terre.jpg">
+    <img src={placeholder} alt="placeholder" width="300" height="200"></img>
+  </a>
+</div>
+</ul>
+
+<ul><div class="gallery">
+  <a target="_blank" href="img_5terre.jpg">
+    <img src={placeholder} alt="placeholder" width="300" height="200"></img>
+  </a>
+</div>
+</ul>
+
+</ul>
+{/* </div> */}
+      <img src={placeholder} alt="placeholder" width="400" height="200"></img>
     </div>
   );
 }
