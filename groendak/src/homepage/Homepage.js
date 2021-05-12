db041eac4f3b4a576d50aacab8522e34b7e176a5
ToastyMartian <cakallemein@gmail.com>
2021-05-12 12:44:22 +0200

    Added carousel to homepage

--- groendak/src/homepage/Homepage.js
+++ groendak/src/homepage/Homepage.js
@@ -1,10 +1,20 @@
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
+
+//images for carousel
+import eco from '../img/eco.png';
+import hellend from '../img/hellend.jpg';
+import licht from '../img/licht.png';
+import natuur from '../img/natuur.jpg';
+import solar from '../img/solar.jpg';
+
 import './Homepage.css';
 
 function Homepage() {
   return (
     <div>
 
-      <section>
+      <section className='p-3'>
         <h3 className='pt-3 text-green-600 font-semibold'>Groene Daken</h3>
         <p className='p-2 max-w-prose m-auto'>
           Groene daken komen in alle maten en vormen, zo zijn er verschillende mogelijkheden voor soorten beplanting en platte en hellende daken. 
@@ -21,12 +31,33 @@ function Homepage() {
         </p>
       </section>
 
-      <section>
-        <h3 className='text-green-600 font-semibold'>Types</h3>
-        <p>[insert cool looking slideshow]</p>
+      <section className='p-3 font-bold'>
+        <h3 className='pb-3 text-green-600 font-semibold'>Types</h3>
+        <AliceCarousel autoPlay autoPlayInterval="3000">
+          <div>
+            <img src={eco} className='m-auto w-2/4 h-auto object-contain' alt='Economisch groen dak'/>
+            <p className='text-green-700 font-semibold'>Economisch Groendak</p>
+          </div>
+          <div>
+            <img src={hellend} className='m-auto w-2/4 h-auto object-contain' alt='Hellend groen dak'/>
+            <p className='text-green-700 font-semibold'>Hellend Groendak</p>
+          </div>
+          <div>
+            <img src={licht} className='m-auto w-2/4 h-auto object-contain' alt='Licht gewicht groen dak'/>
+            <p className='text-green-700 font-semibold'>Lichtgewicht Groendak</p>
+          </div>
+          <div>
+            <img src={natuur} className='m-auto w-2/4 h-auto object-contain' alt='Natuur groen dak'/>
+            <p className='text-green-700 font-semibold'>Natuur Groendak</p>
+          </div>
+          <div>
+            <img src={solar} className='m-auto w-2/4 h-auto object-contain' alt='Solar groen dak'/>
+            <p className='text-green-700 font-semibold'>Solar Groendak</p>
+          </div>
+        </AliceCarousel>
       </section>
 
-      <section>
+      <section className='p-3'>
         <h3 className='text-green-600 font-semibold'>Stappenplan</h3>
         <p className='p-2 max-w-prose m-auto'>
           Binnen drie stappen kunt u gratis uitvinden wat voor groendak het best bij uw wensen en situatie zou passen en hoe u hiermee aan de slag zou kunnen. 
@@ -34,14 +65,14 @@ function Homepage() {
         </p>
       </section>
 
-      <section>
+      <section className='p-3'>
         <h3 className='text-green-600 font-semibold'>Stap 1: Formulier</h3>
         <p className='p-2 max-w-prose m-auto'>
           In het formulier vult u uw gegevens en voorkeuren in zodat wij een advies op maat kunnen geven. 
         </p>
       </section>
 
-      <section>
+      <section className='p-3'>
         <h3 className='text-green-600 font-semibold'>Stap 2: Opties</h3>
         <p className='p-2 max-w-prose m-auto'>
           Op basis van uw gegevens en voorkeuren worden er een aantal daken weergegeven en waarom deze bij u zouden passen. 
@@ -49,7 +80,7 @@ function Homepage() {
         </p>
       </section>
 
-      <section>
+      <section className='p-3'>
         <h3 className='text-green-600 font-semibold'>Stap 3: Vervolgstappen</h3>
         <p className='p-2 max-w-prose m-auto'>
           Hier vind u alle informatie over het gekozen dak en hoe u hiermee aan de slag zou kunnen.
