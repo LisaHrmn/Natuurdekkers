a28ed49be8c9de8a35e12892231dda33101b4c2c
Basri <basrietkerk@hotmail.nl>
2021-05-18 12:04:38 +0200

    form (broken)

--- groendak/src/stap1/stap1.js
+++ groendak/src/stap1/stap1.js
@@ -8,6 +8,7 @@ const knn = new generator.kNear(k);
 let gewichtEco;
 let gewichtLicht;
 let gewichtNatuur;
+
 let i;
 for (i = 0; i < 100; i++) {
   gewichtEco = Math.floor(Math.random() * (10 - 8 + 1)) + 8;
@@ -19,13 +20,41 @@ for (i = 0; i < 100; i++) {
   knn.learn([gewichtNatuur, 3, 5, 2], "natuur");
 }
 
-let prediction = knn.classify([9, 3, 5, 2]);
-console.log(prediction)
+document.getElementById('submit').onclick = function(event) {
+  event.preventDefault();
+  let gewicht = document.getElementById("gewicht").value;
+  let water = document.getElementById("water").value;
+  let bio = document.getElementById("bio").value;
+  let onderhoud = document.getElementById("onderhoud").value;
+
+  gewicht = parseInt(gewicht);
+  gewicht = gewicht / 10;
+  water = parseInt(water);
+  bio = parseInt(bio);
+  onderhoud = parseInt(onderhoud);
+
+  let prediction = knn.classify([gewicht, water, bio, onderhoud])
+  console.log(prediction)
+}
 
 function Stap1() {
   return (
     <div>
-      <h1>This is Step one</h1>
+      <form action="#">
+        <label htmlFor="gewicht">Gewicht in KG</label>
+        <input id="gewicht" type="number"/>
+        <br/>
+        <label htmlFor="water">1-3</label>
+        <input id="water" type="number"/>
+        <br/>
+        <label htmlFor="bio">biodiversiteit 1-5</label>
+        <input id="bio" type="number" min="1" max="5"/>
+        <br/>
+        <label htmlFor="onderhoud">onderhoud 1-5</label>
+        <input id="onderhoud" type="number" min="1" max="5"/>
+        <br/>
+        <button id="submit">Submit</button>
+      </form>
     </div>
   );
 }
