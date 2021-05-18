a6f63328b660b0923d3311d84bf435225ec6c2be
Dennis van Britsem <dvbritsem@gmail.com>
2021-05-18 12:30:25 +0200

    Added form

--- groendak/src/stap1/stap1.js
+++ groendak/src/stap1/stap1.js
@@ -1,62 +1,72 @@
 import './stap1.css';
+import React, { Component } from 'react';
 
-const generator = require('knear');
+class Stap1 extends Component {
+    constructor() {
+      super()
+      this.handleSubmit = this.handleSubmit.bind(this);
 
-const k = 3;
-const knn = new generator.kNear(k);
+      const generator = require('knear');
 
-let gewichtEco;
-let gewichtLicht;
-let gewichtNatuur;
+      const k = 3;
+      const knn = new generator.kNear(k);
+    
+      let gewichtEco;
+      let gewichtLicht;
+      let gewichtNatuur;
 
-let i;
-for (i = 0; i < 100; i++) {
-  gewichtEco = Math.floor(Math.random() * (10 - 8 + 1)) + 8;
-  gewichtLicht = Math.floor(Math.random() * (6.5 - 4.5 + 1)) + 4.5;
-  gewichtNatuur = Math.floor(Math.random() * (10 - 8 + 1)) + 8;
+      let i;
+      for (i = 0; i < 100; i++) {
+        gewichtEco = Math.floor(Math.random() * (10 - 8 + 1)) + 8;
+        gewichtLicht = Math.floor(Math.random() * (6.5 - 4.5 + 1)) + 4.5;
+        gewichtNatuur = Math.floor(Math.random() * (10 - 8 + 1)) + 8;
+    
+        knn.learn([gewichtEco, 2, 2, 1], "eco");
+        knn.learn([gewichtLicht, 1, 1, 2], "licht");
+        knn.learn([gewichtNatuur, 3, 5, 2], "natuur");
+      }
 
-  knn.learn([gewichtEco, 2, 2, 1], "eco");
-  knn.learn([gewichtLicht, 1, 1, 2], "licht");
-  knn.learn([gewichtNatuur, 3, 5, 2], "natuur");
-}
+      this.state = {knn: knn};
+    }
 
-document.getElementById('submit').onclick = function(event) {
-  event.preventDefault();
-  let gewicht = document.getElementById("gewicht").value;
-  let water = document.getElementById("water").value;
-  let bio = document.getElementById("bio").value;
-  let onderhoud = document.getElementById("onderhoud").value;
+    handleSubmit(event) {
+      event.preventDefault();
+      let gewicht = document.getElementById("gewicht").value;
+      let water = document.getElementById("water").value;
+      let bio = document.getElementById("bio").value;
+      let onderhoud = document.getElementById("onderhoud").value;
+  
+      gewicht = parseInt(gewicht);
+      gewicht = gewicht / 10;
+      water = parseInt(water);
+      bio = parseInt(bio);
+      onderhoud = parseInt(onderhoud);
+  
+      let prediction = this.state.knn.classify([gewicht, water, bio, onderhoud])
+      console.log(prediction)
+    }
 
-  gewicht = parseInt(gewicht);
-  gewicht = gewicht / 10;
-  water = parseInt(water);
-  bio = parseInt(bio);
-  onderhoud = parseInt(onderhoud);
-
-  let prediction = knn.classify([gewicht, water, bio, onderhoud])
-  console.log(prediction)
-}
-
-function Stap1() {
-  return (
-    <div>
-      <form action="#">
-        <label htmlFor="gewicht">Gewicht in KG</label>
-        <input id="gewicht" type="number"/>
-        <br/>
-        <label htmlFor="water">1-3</label>
-        <input id="water" type="number"/>
-        <br/>
-        <label htmlFor="bio">biodiversiteit 1-5</label>
-        <input id="bio" type="number" min="1" max="5"/>
-        <br/>
-        <label htmlFor="onderhoud">onderhoud 1-5</label>
-        <input id="onderhoud" type="number" min="1" max="5"/>
-        <br/>
-        <button id="submit">Submit</button>
-      </form>
-    </div>
-  );
+    render() {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="gewicht">Gewicht in KG</label>
+            <input id="gewicht" type="number"/>
+            <br/>
+            <label htmlFor="water">1-3</label>
+            <input id="water" type="number"/>
+            <br/>
+            <label htmlFor="bio">biodiversiteit 1-5</label>
+            <input id="bio" type="number" min="1" max="5"/>
+            <br/>
+            <label htmlFor="onderhoud">onderhoud 1-5</label>
+            <input id="onderhoud" type="number" min="1" max="5"/>
+            <br/>
+            <button id="submit">Submit</button>
+          </form>
+        </div>
+      );
+    }
 }
 
 export default Stap1;
