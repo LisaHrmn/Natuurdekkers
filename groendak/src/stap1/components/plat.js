99dc62fcd6cd74185b75abcb916a5d223a2e6356
Basri <basrietkerk@hotmail.nl>
2021-05-25 12:54:10 +0200

    added extra input field

--- groendak/src/stap1/components/plat.js
+++ groendak/src/stap1/components/plat.js
@@ -15,19 +15,22 @@ class Plat extends Component {
     let gewichtEco;
     let gewichtLicht;
     let gewichtNatuur;
+    let gewichtSolar;
 
     let i;
     for (i = 0; i < 100; i++) {
-      gewichtEco = Math.floor(Math.random() * (10 - 8 + 1)) + 8;
-      gewichtLicht = Math.floor(Math.random() * (6.5 - 4.5 + 1)) + 4.5;
+      gewichtEco = Math.floor(Math.random() * (7.5 - 6.5 + 1)) + 6.5;
+      gewichtLicht = Math.floor(Math.random() * (6.5 - 4 + 1)) + 4;
       gewichtNatuur = Math.floor(Math.random() * (10 - 8 + 1)) + 8;
-  
+      gewichtSolar = Math.floor(Math.random() * (12.5 - 10 + 1)) + 10;
+
       knn.learn([gewichtEco, 2, 2, 1], "eco");
       knn.learn([gewichtLicht, 1, 1, 2], "licht");
       knn.learn([gewichtNatuur, 3, 5, 2], "natuur");
+      knn.learn([gewichtSolar, 2, 2, 2], "solar");
     }
 
-    this.state = {knn: knn, recommendation: "", done: false};
+    this.state = {knn: knn, recommendation: "", prijsMin: "", prijsMax: "", done: false};
   }
 
   handleSubmit(event) {
@@ -36,6 +39,10 @@ class Plat extends Component {
     let water = document.getElementById("water").value;
     let bio = document.getElementById("bio").value;
     let onderhoud = document.getElementById("onderhoud").value;
+    let oppervlak = document.getElementById("oppervlak").value;
+
+    let prijsMin;
+    let prijsMax;
 
     gewicht = parseInt(gewicht);
     gewicht = gewicht / 10;
@@ -46,8 +53,31 @@ class Plat extends Component {
     let prediction = this.state.knn.classify([gewicht, water, bio, onderhoud])
     console.log(prediction)
 
+    if(prediction === "licht")
+    {
+      prijsMin = oppervlak * 35;
+      prijsMax = oppervlak * 60;
+    }
+    else if(prediction === "eco")
+    {
+      prijsMin = oppervlak * 25;
+      prijsMax = oppervlak * 55;
+    }
+    else if(prediction === "natuur")
+    {
+      prijsMin = oppervlak * 45;
+      prijsMax = oppervlak * 80;
+    }
+    else if(prediction === "solar")
+    {
+      prijsMin = oppervlak * 25;
+      prijsMax = oppervlak * 25;
+    }
+
     this.setState({
       recommendation: prediction,
+      prijsMin: prijsMin,
+      prijsMax: prijsMax,
       done: true
     }, () => {
       return this.state;
@@ -61,7 +91,7 @@ class Plat extends Component {
           <Redirect
             to={{
               pathname: "/2",
-              state: { recommendation: this.state.recommendation },
+              state: { recommendation: this.state.recommendation, prijsMin: this.state.prijsMin, prijsMax: this.state.prijsMax },
             }}
           />
         </div>
@@ -74,6 +104,9 @@ class Plat extends Component {
           <form onSubmit={this.handleSubmit}>
             <label htmlFor="gewicht">Gewicht in KG</label>
             <input id="gewicht" type="number"/>
+            <br/>
+            <label htmlFor="oppervlak">Oppervlakte dak in m²</label>
+            <input id="oppervlak" type="number"/>
             <br/>
             <label htmlFor="water">1-3</label>
             <input id="water" type="number"/>
--- groendak/src/stap2/stap2.js
+++ groendak/src/stap2/stap2.js
@@ -12,6 +12,8 @@ class Stap2 extends Component {
     return (
       <div>
         <h2>Aanbeveling: {this.props.location.state.recommendation}</h2>
+        <h3>Min prijs: {this.props.location.state.prijsMin}</h3>
+        <h3>Max prijs: {this.props.location.state.prijsMax}</h3>
       </div>
     );
   }
