4ae282f65ad4469c070da1beb67ac40565e83a9d
ToastyMartian <cakallemein@gmail.com>
2021-06-08 13:36:21 +0200

    Edited layout and contents of Handleiding.js and made small changes to responsiveness of Header.js

--- groendak/src/Handleiding/handleiding.js
+++ groendak/src/Handleiding/handleiding.js
@@ -1,101 +1,116 @@
 function Handleiding() {
-    return(<div >
-        <h1>Handleiding</h1><br></br>
-        <h2>Stap 1 beoordeel of uw dak geschikt is</h2>
-        <p>De verschillende groendaken hebben een verschillend gewicht, het is belangrijk dat u weet of uw dak het gewicht aankan. Bij elk groendak wordt het gewicht aangegeven.
-Ook moet u rekening houden met de dakhelling, als het dak schuin is kunnen hier maatregelen moeten worden genomen om een groendak mogelijk te maken. 
-Een groendak is mogelijk als uw dak een vlakke stabiele ondergrond biedt, zoals van beton of bitumen. Een dak van dakpannen is niet geschikt, u kunt de dakpannen wel vervangen door een andere dakbedekking en op die nieuwe ondergrond vervolgens een groendak aanleggen.
-Als u al andere bevestigingen op het dak heeft zoals zonnepanelen, ventilatie, ramen, opslag of een boom dan is een groendak in de meeste gevallen zeker nog mogelijk. Denk hierbij er wel aan dat dit extra onderhoud eventueel nodig kan zijn. 
-</p><br></br>
-<h2>Stap 2 kies een groendak dat bij u past </h2>
-<p>Middels het <a className='text-blue-500' href='/stap0'>stappenplan</a> op deze website kunt u gemakkelijk door middel van een AI uitvinden welk dak het beste bij u situatie en wensen past. Wij slaan u data niet op, wat u invult op het formulier is alleen zichtbaar en beschikbaar voor uzelf. 
-</p><br></br>
+    return(
+        <div className='ml-20 max-w-3/4 pt-10'>
+            <h1 className='font-semibold text-green-600'>Handleiding</h1>
+            
+            <h2 className='pt-10'>Stap 1: Beoordeel of uw dak geschikt is</h2>
+                <p className='pt-5'>
+                    De verschillende groendaken hebben een verschillend gewicht, het is belangrijk dat u weet of uw dak het gewicht aankan. Bij elk groendak wordt het gewicht aangegeven.
+                    Ook moet u rekening houden met de dakhelling, als het dak schuin is kunnen hier maatregelen moeten worden genomen om een groendak mogelijk te maken. 
+                    Een groendak is mogelijk als uw dak een vlakke stabiele ondergrond biedt, zoals van beton of bitumen. Een dak van dakpannen is niet geschikt, u kunt de dakpannen wel vervangen door een andere dakbedekking en op die nieuwe ondergrond vervolgens een groendak aanleggen.
+                    Als u al andere bevestigingen op het dak heeft zoals zonnepanelen, ventilatie, ramen, opslag of een boom dan is een groendak in de meeste gevallen zeker nog mogelijk. Denk hierbij er wel aan dat dit extra onderhoud eventueel nodig kan zijn. 
+                </p>
+            
+            <h2 className='pt-10'>Stap 2: Kies een groendak dat bij u past </h2>
+                <p className='pt-5'>
+                    Middels het <a className='text-green-600 hover:text-green-700 font-semibold' href='/stap0'>stappenplan</a> op deze website kunt u gemakkelijk door middel van een AI uitvinden welk dak het beste bij uw situatie en wensen past. 
+                    Wij slaan uw data niet op, wat u invult op het formulier is alleen zichtbaar en beschikbaar voor uzelf. 
+                </p>
 
-<h2>Stap 3 schaf de nodige materialen aan</h2>
-<p>De opties die wij u aanraden na het stappenplan zijn voornamelijk voordelige pakketten waarin alles zit wat u nodig heeft. Vaak bestaat een groendak uit de volgende onderdelen:
-</p><br></br>
+            <h2 className='pt-10'>Stap 3: Schaf de nodige materialen aan</h2>
+                <p className='pt-5'>
+                    De opties die wij u aanraden na het stappenplan zijn voornamelijk voordelige pakketten waarin alles zit wat u nodig heeft. Vaak bestaat een groendak uit de volgende onderdelen:
+                </p>
+                <ol className='ml-5'>
+                    <li class='list-disc pt-5'>Afvoer Bescherming</li>
+                    <p>{'>'} Als u op het dak een waterafvoer heeft, heeft deze afvoer bescherming nodig, zodat deze niet verstopt raakt.</p>
 
-    <li class='list-disc'>Afvoer Bescherming <br></br>
- {'>'}als u op het dak een waterafvoer heeft, heeft u een afvoer bescherming nodig zodat deze niet verstopt raakt.
- <li>Beschermdoek <br></br>
- {'>'}wordt toegepast om de dakbedekking te beschermen
-</li>
-<li>Drainagelaag <br></br>
-{'>'}voor buffering van water zodat deze in droge periodes opgenomen kan worden en snellere afvoer van water in natte periodes.
-</li>
-<li>Filterlaag<br></br>
-{'>'} scheidt de drainagelaag en de substraatlaag. Dit voorkomt dat kleine gronddeeltjes de drainage vullen. Zo blijft de waterbufferende functie van de drainage beter intact.  
-</li>
-<li>Substraatlaag<br></br>
-{'>'}de voedingsbodem voor de planten  
-</li>
-<li>
-Vegetatielaag<br></br>
-{'>'}de planten.
-</li>
-<li>
-Grindrand <br></br>
-{'>'}is nodig langs de dakrand voor betere waterafvoer. Het soort grind dat wordt aangeraden is grof rond grind.
-</li>
-</li> <br></br>
-In sommige gevallen zijn meerdere onderdelen gecombineerd.<br></br>
-In sommige pakketten ontbreekt een afvoer bescherming en grind.<br></br>
+                    <li class='list-disc pt-5'>Beschermdoek</li>
+                    <p>{'>'} Dit wordt toegepast om de dakbedekking te beschermen</p>
 
-<h2>Stap 4 leg het groendak aan</h2>
+                    <li class='list-disc pt-5'>Drainagelaag</li>
+                    <p>{'>'} Voor de buffering van water, zodat deze in droge periodes opgenomen kan worden en in natte periodes sneller afgevoerd wordt.</p>
+                    
+                    <li class='list-disc pt-5'>Filterlaag</li>
+                    <p>{'>'} Dit scheidt de drainagelaag en de substraatlaag. Dit voorkomt dat kleine gronddeeltjes de drainage vullen. Zo blijft de waterbufferende functie van de drainage beter intact.</p>  
 
-<p>U kunt een aannemer inhuren om het groendak aan te leggen, maar dit is ook vrij gemakkelijk zelf te doen! Het volgende stappenplan kan u opweg helpen. Ook is deze video van groendakcoach handig om de instructies te zien.</p>
-<br></br>
-<p>STAP 1 – Schoonmaken<br></br>
-Zorg dat het dak vrij is van vuil, afval of andere materialen. Controleer de dakbedekking voor beschadigingen.
-</p><br></br>
-<p>STAP 2 – Afvoer bescherming (optioneel)<br></br>
-Als het dak een waterafvoer of regenpijp heeft is er een afvoer bescherming nodig. Plaatse de afvoer bescherming over de waterafvoer.
-</p><br></br>
-<p>STAP 3 - Beschermdoek<br></br>
-Breng de beschermdoek aan op het dak. Zorg dat de beschermdoeken goed op elkaar aansluiten door ze voor een gedeelte te overlappen (tot 50cm). 
-</p><br></br>
-<p>STAP 4 – Drainagelaag <br></br>
-Leg de drainagelagen haaks op de beschermdoeken. Waar nodig, snij of skip de drainagelaag veilig op maat, snij nooit direct op de dakbedekking. Als u het groendak in een droge of warme periode aanlegt, sproei deze laag dan alvast flink met water. 
-</p><br></br>
-<p>STAP 5 – Filterlaag<br></br>
-Breng de filterlaag aan op de drainagelaag. Waar nodig is deze op maat te knippen of snijden.
-</p><br></br>
-<p>STAP 6 – Substraatlaag<br></br>
-Verdeel de substraatlaag zo gelijkmatig mogelijk over het dak. 
-Als u substraat rollen heeft, leg deze haaks op de filterlaag en drainagelaag. Zorg dat de substraat rollen zonder overlap goed op elkaar aansluiten. 
-Als u het groendak in een droge of warme periode aanlegt, sproei deze laag dan alvast flink met water. 
-Laat een rand vrij van minimaal 10cm bij de dakrand voor het grind.
-</p><br></br>
-<p>STAP 7 – Vegetatielaag<br></br>
-Het is ook mogelijk om stap 8, het grind eerst te doen.
-Poot de planten in de substraatlaag. 
-Als u vegetatiematten heeft, rol deze haaks over de substraat rollen. Start met het uiteinde van de vegetatiemat bovenop. Rol het uiteinde van de mat voorzichtig terug en zorg dat deze op de juiste positie ligt aan het begin van de baan. Indien nodig, knip of snij de mat aan het eind van de baan op maat. Zorg dat de vegetatiematten zonder overlap goed op elkaar aansluiten. Eventuele kale plekken kunnen opgevuld worden met reststukken van de vegetatiemat.
-Laat een rand vrij van minimaal 10cm bij de dakrand voor het grind.
+                    <li class='list-disc pt-5'>Substraatlaag</li>
+                    <p>{'>'} De voedingsbodem voor de beplanting van het groen dak</p>
 
-</p><br></br>
-<p>STAP 8 – Grind<br></br>
-Dit is ook mogelijk te doen voor stap 7, de vegetatielaag.
-Breng het grind aan bij de dakrand die u heeft vrijgelaten. De grindlaag kan worden aangevuld tot gelijke hoogte van de dakrand, substraat en vegetatielaag.
-</p><br></br>
-<p>
-STAP 9 – Irrigeren<br></br>
-Bewater direct na het aanleggen van alles de vegetatie totdat de substraatlaag volledig bewaterd is. Dit is nodig voor de doorworteling van de vegetatie.
-</p><br></br>
-<h2>Stap 5 onderhoud</h2>
-<p>Er zijn een aantal regels voor standaard onderhoud van een groendak. </p>
-<li class='list-disc'>
-Verwijderen van ongewenste onkruiden en zaailingen van bomen uit de vegetatie (2 keer per jaar)
-<li>Verwijderen van Sedum en onkruiden uit de grindranden</li>
-<li>Bemesten met speciaal samengestelde meststof (1 keer per jaar, ca 25-30 gram/m2)
-</li>
-<li>Schoonmaken van de hemelwaterafvoer</li>
-<li>Bij droge perioden extra water geven, voor de rest is dit niet nodig.
-</li>
+                    <li class='list-disc pt-5'>Vegetatielaag</li>
+                    <p>{'>'} De beplanting van het groen dak.</p>
 
-</li><br></br>
-<p>Voor sommige groendaken als het natuur groendak is meer onderhoud nodig, deze heeft wat vaker water nodig en verwijdering van onkruid.</p>
+                    <li class='list-disc pt-5'>Grindrand</li>
+                    <p>{'>'} Dit is nodig langs de dakrand voor betere waterafvoer. Het soort grind dat wordt aangeraden is grof, rond grind.</p>
+                </ol>
+                <p className='pt-5'>
+                    In sommige gevallen zijn meerdere onderdelen gecombineerd.
+                </p>
+                <p className='pt-5'>
+                    In sommige pakketten ontbreekt een afvoer bescherming en grind.
+                </p>
 
-    </div>);
+            <h2 className='pt-10'>Stap 4: Leg het groendak aan</h2>
+                <p className='pt-5'>
+                    U kunt een aannemer inhuren om het groendak aan te leggen, maar dit is ook vrij gemakkelijk zelf te doen! 
+                    Het volgende stappenplan kan u opweg helpen. Ook is deze video van groendakcoach handig om de instructies te zien.
+                </p>
+                <h5 className='pt-5'>STAP 1 – Schoonmaken</h5>
+                <p>
+                    Zorg dat het dak vrij is van vuil, afval of andere materialen. Controleer de dakbedekking voor beschadigingen.
+                </p>
+                <h5 className='pt-5'>STAP 2 – Afvoer bescherming (optioneel)</h5>
+                <p>
+                    Als het dak een waterafvoer of regenpijp heeft, is er een afvoer bescherming nodig. Plaats de afvoer bescherming over de waterafvoer.
+                </p>
+                <h5 className='pt-5'>STAP 3 - Beschermdoek</h5>
+                <p>
+                    Breng de beschermdoek aan op het dak. Zorg dat de beschermdoeken goed op elkaar aansluiten door ze voor een gedeelte te overlappen (tot 50cm). 
+                </p>
+                <h5 className='pt-5'>STAP 4 – Drainagelaag</h5>
+                <p>
+                    Leg de drainagelagen haaks op de beschermdoeken. Indien nodig, snij of skip de drainagelaag veilig op maat, snij nooit direct op de dakbedekking. Als u het groendak in een droge of warme periode aanlegt, sproei deze laag dan alvast flink met water. 
+                </p>
+                <h5 className='pt-5'>STAP 5 – Filterlaag</h5>
+                <p>
+                    Breng de filterlaag aan op de drainagelaag. Waar nodig is deze op maat te knippen of snijden.
+                </p>
+                <h5 className='pt-5'>STAP 6 – Substraatlaag</h5>
+                <p>
+                    Verdeel de substraatlaag zo gelijkmatig mogelijk over het dak. 
+                    Als u substraat rollen heeft, leg deze haaks op de filterlaag en drainagelaag. Zorg dat de substraat rollen zonder overlap goed op elkaar aansluiten. 
+                    Als u het groendak in een droge of warme periode aanlegt, sproei deze laag dan alvast flink met water. 
+                    Laat een rand vrij van minimaal 10cm bij de dakrand voor het grind.
+                </p>
+                <h5 className='pt-5'>STAP 7 – Vegetatielaag</h5>
+                <p>
+                    Het is ook mogelijk om stap 8, het grind eerst te doen.
+                    Poot eerst de planten in de substraatlaag. 
+                    Als u vegetatiematten heeft, rol deze haaks over de substraat rollen. Start met het uiteinde van de vegetatiemat bovenop. Rol het uiteinde van de mat voorzichtig terug en zorg dat deze op de juiste positie ligt aan het begin van de baan. Indien nodig, knip of snij de mat aan het eind van de baan op maat. Zorg dat de vegetatiematten zonder overlap goed op elkaar aansluiten. Eventuele kale plekken kunnen opgevuld worden met reststukken van de vegetatiemat.
+                    Laat een rand vrij van minimaal 10cm bij de dakrand voor het grind.
+                </p>
+                <h5 className='pt-5'>STAP 8 – Grind</h5>
+                <p>
+                    Dit is ook mogelijk te doen voor stap 7, de vegetatielaag.
+                    Breng het grind aan bij de dakrand die u heeft vrijgelaten. De grindlaag kan worden aangevuld tot gelijke hoogte van de dakrand, substraat en vegetatielaag.
+                </p>
+                <h5 className='pt-5'>STAP 9 – Irrigeren</h5>
+                <p>
+                    Bewater direct na het aanleggen van alles de vegetatie totdat de substraatlaag volledig bewaterd is. Dit is nodig voor de doorworteling van de vegetatie.
+                </p>
+
+            <h2 className='pt-10'>Stap 5: Onderhoud</h2>
+                <p className='pt-5 pb-5'>Er zijn een aantal regels voor standaard onderhoud van een groendak.</p>
+                <ol className='ml-5'>
+                    <li class='list-disc'>Verwijderen van ongewenste onkruiden en zaailingen van bomen uit de vegetatie (2 keer per jaar)</li>
+                    <li class='list-disc'>Verwijderen van Sedum en onkruiden uit de grindranden</li>
+                    <li class='list-disc'>Bemesten met speciaal samengestelde meststof (1 keer per jaar, ca 25-30 gram/m2)</li>
+                    <li class='list-disc'>Schoonmaken van de hemelwaterafvoer</li>
+                    <li class='list-disc'>Bij droge perioden extra water geven, voor de rest is dit niet nodig.</li>
+                </ol>
+            <p className='pt-5 pb-10'>Voor sommige groendaken, zoals het natuur groendak, is meer onderhoud nodig. Deze heeft bijvoorbeeld wat vaker water nodig en onderhoud zoals het verwijderen van onkruid.</p>
+        </div>
+    );
 }
 
 export default Handleiding;
--- groendak/src/components/Header/Header.js
+++ groendak/src/components/Header/Header.js
@@ -1,9 +1,9 @@
 function Header() {
     return (
       <div className='pt-6 pb-7 bg-green-800 text-center'>
-          <header className='m-auto flex justify-between'>
-            <a href="/"><h1 className='pl-20 font-semibold text-gray-100'>Natuurdekkers</h1></a>
-            <ul className='mr-20 flex justify-evenly'>
+          <header className='m-auto sm:flex justify-between'>
+            <a href="/"><h1 className='sm:pl-20 font-semibold text-gray-100'>Natuurdekkers</h1></a>
+            <ul className='sm:mr-20 flex justify-evenly'>
               <li className='flex self-center hover:text-gray-100'>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
